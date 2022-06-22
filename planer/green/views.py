f4b39e3421ee956eda01a25a977fc87c70218d17
nacia11454 <natalia.bartos@student.wat.edu.pl>
2022-06-22 13:03:42 +0200

    add calendar

--- planer/green/views.py
+++ planer/green/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render
+import calendar
+from calendar import HTMLCalendar
+from datetime import datetime
 
-def home(request):
-    return render(request,'home.html', {})
+def home(request, year=datetime.now().year, month=datetime.now().strftime('%B')):
+    month = month.capitalize()
+    #conwert month from name to number
+    month_number = list(calendar.month_name).index(month)
+    month_number=int(month_number)
+
+    #create calendar
+    cal = HTMLCalendar().formatmonth(year,month_number)
+
+    #get current year
+    now = datetime.now()
+    current_year = now.year
+    # get current year
+    time= now.strftime('%H:%M')
+
+    return render(request,'home.html', {
+        "year":year,
+        "month":month,
+        "month_number":month_number,
+        "cal":cal,
+        "current_year":current_year,
+        "time":time,
+    })
