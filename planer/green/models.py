a3fe0357671e5fa8b5a600b8c7b1da624432c4e6
nacia11454 <natalia.bartos@student.wat.edu.pl>
2022-06-22 12:27:46 +0200

    init commit

---
+++ planer/green/models.py
@@ -0,0 +1,17 @@
+# from django.db import models
+#
+# class Plant(models.Model):
+#     name_pl = models.CharField()
+
+
+
+
+    # class Plant(models.Model):
+    #     group = models.ForeignKey(Group, on_delete=models.CASCADE)
+    #     name_pl = models.CharField(max_length=100)
+    #     sun = models.IntegerField(default=5)
+    #     water = models.IntegerField(default=5)
+    #     likes = models.IntegerField(default=0)
+    #
+    #     def __str__(self):
+    #         return self.name_pl
